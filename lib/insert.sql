9389eed86d52f5ae8d2daef5b05118b66681d6a7
Evan Chernicky <echernicky@gmail.com>
2022-03-19 20:39:42 -0400

    commit tree 72340cf6ccf4e065c51d584ce193deb103239c50

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES  
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'cute', true),
+    ('Rowdy', 19, 'M', 'White', 'wild', true),
+    ('Tabitha', 89, 'F', 'Gray', 'shy', false),
+    ('Sergeant Brown', 28, 'M', 'Brown', 'looking to fight', false),
+    ('Melissa', 4, 'F', 'Purple', 'Social', true),
+    ('Grinch', 55, 'M', 'Green', 'Growchy', false),
+    ('Wendy', 33, 'F', 'Orange', 'Nice', true),
+    (NULL, 22, 'M', NULL, NULL, false);
